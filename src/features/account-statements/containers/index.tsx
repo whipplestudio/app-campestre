b7d22306bc4b2f7a47c1fa8b57006fc22bf0a3e9
YamilethMC <L21170016@culiacan.tecnm.mx>
2025-10-21 20:38:44 -0700

    Estructura de carpeta de estados de cuenta

---
+++ src/features/account-statements/containers/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const AccountStatementsContainer = () => {
+
+  return (
+    <View>
+      <Text>AccountStatementsContainer</Text>
+    </View>
+  );
+};
+
+export default AccountStatementsContainer;
